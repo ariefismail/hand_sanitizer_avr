3134e7975707ebd1b5be49205c07d64d102ddc9b
Arief Ismail <ismail.pratama@formulatrix.com>
2020-03-29 12:25:05 +0700

    initial commit

---
+++ hand_sanitizier/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * hand_sanitizier.cpp
+ *
+ * Created: 3/29/2020 11:11:40 AM
+ * Author : Formulatrix
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
